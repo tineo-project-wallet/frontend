e4a9bf8268c9a1e16c037a018de8fe16d55a3ce5
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-13 15:28:09 -0500

    feat: add router list

---
+++ src/routes/routeList.js
@@ -0,0 +1,12 @@
+export const basicRouteList = {
+  home: '/',
+  login: '/login',
+  register: '/register',
+  profile: '/profile',
+  notFound: '*'
+}
+
+export const adminRouteList = {
+  dashboard: '/admin',
+  users: '/admin/users'
+}
