1a40c7f39cdac26bba59847d10805bf8942899b5
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-13 16:06:36 -0500

    feat: set layout to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
 import { basicRouteList, adminRouteList } from "./routes/routeList.js";
+import LoggedLayout from "./components/layout/logged/LoggedLayout.jsx";
 
 function App() {
   return (
     <>
-      <h1>Wallet - Header</h1>
-
       <Routes>
-        <Route path={basicRouteList.home} element={<h1>Home</h1>} />
         <Route path={basicRouteList.login} element={<h1>Login</h1>} />
         <Route path={basicRouteList.register} element={<h1>Register</h1>} />
-        <Route path={basicRouteList.profile} element={<h1>Profile</h1>} />
 
-        <Route path={adminRouteList.dashboard} element={<h1>Dashboard</h1>} />
-        <Route path={adminRouteList.users} element={<h1>Users</h1>} />
+        <Route element={<LoggedLayout />}>
+          <Route path={basicRouteList.home} element={<h1>Home</h1>} />
+          <Route path={basicRouteList.profile} element={<h1>Profile</h1>} />
+
+          <Route path={adminRouteList.dashboard} element={<h1>Dashboard</h1>} />
+          <Route path={adminRouteList.users} element={<h1>Users</h1>} />
+        </Route>
 
         <Route path={basicRouteList.notFound} element={<h1>Not Found</h1>} />
       </Routes>
-
-      <p>Wallet - Footer</p>
     </>
   );
 }
