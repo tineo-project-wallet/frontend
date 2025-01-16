a35d38f272ac0ad48856f7bd166486a01817f287
tineojf <92743762+tineodev@users.noreply.github.com>
2025-01-16 11:50:44 -0500

    feat: add login component structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+import { basicRouteList } from "../routes/routeList";
+
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <h1>Wallet</h1>
+
+      <h2>Welcome back</h2>
+
+      <form method="post">
+        <label htmlFor="email">
+          Email address
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="demo@gmail.com"
+            required
+          />
+        </label>
+
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="*************"
+            required
+          />
+        </label>
+
+        <button type="submit">Sign in</button>
+      </form>
+
+      <p>
+        <Link to={basicRouteList.register}>Don&apos;t have an account?</Link>
+      </p>
+    </>
+  );
 }
 
-export default Login
+export default Login;
